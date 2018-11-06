89155ab881c1b73b9dadf801594913f776b23e40
Fei Gao <fgao1@ualberta.ca>
2018-11-06 14:57:47 -0700

    add introduction section

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $(".sidenav").sidenav();
+  $(".parallax").parallax();
+  $(".tabs").tabs();
+  $(".scrollspy").scrollSpy();
+  $(".datepicker").datepicker({
+    disableWeekends: true,
+    yearRange: 1
+  });
+});
